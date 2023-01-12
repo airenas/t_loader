faf575bc9c3f4a8de8f469bc1a2b434992eaf43a
Airenas Vaičiūnas <airenass@gmail.com>
2023-01-12 19:59:36 +0200

    Cache ids

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@ python_cmd=PYTHONPATH=./ LOG_LEVEL=INFO python
 n?=10
 version?=0.1
 final_file=$(CURDIR)/nta-$(version)-$(shell date +'%y-%m-%d_%H-%M').zip
-courts_done_files=$(patsubst %,$(work_dir)/%/.done, $(courts))
+courts_done_files=$(patsubst %,$(work_dir)/corpus/%/.done, $(courts))
+top?=0
 ############################################
 ${work_dir}:
 	mkdir -p $@	
@@ -18,16 +19,15 @@ install/req:
 ############################################
 ${work_dir}/.done: $(courts_done_files) | ${work_dir}
 	touch $@
-${work_dir}/%/.done: 
-	mkdir -p $(work_dir)/$*/
-	$(python_cmd) src/download.py --url $(url) --out_dir ${work_dir}/$* --user ${user} --password ${pass} --domain ${domain} --n ${n} \
-		--court $*
+${work_dir}/corpus/%/.done: | ${work_dir}
+	$(python_cmd) src/download.py --url $(url) --out_dir ${work_dir} --user ${user} --password ${pass} --domain ${domain} --n ${n} \
+		--court $* --top $(top)
 	touch $@
 dwn: ${work_dir}/.done	
 ############################################
 ############################################
-$(final_file): $(work_dir)/.done $(work_dir)/info.txt
-	cd $(work_dir); zip -r $@ *
+$(final_file): $(work_dir)/.done $(work_dir)/corpus/info.txt
+	cd $(work_dir)/corpus; zip -r $@ *
 zip: $(final_file)	
 	echo "Final file is: $(final_file)"
 ############################################
@@ -35,15 +35,19 @@ debug:
 	echo "$(courts)"
 	echo "$(courts_done_files)"
 ############################################
-$(work_dir)/info.txt: $(work_dir)/.done
+$(work_dir)/corpus/info.txt: $(work_dir)/.done
 	@printf "NTA texts\n----------------\n" > $@
 	@printf "version     : $(version)\n" >> $@
 	@printf "prepared at : `date +'%Y-%m-%d %H:%M:%S'`\n" >> $@
-	@printf "total files : `ls $(work_dir)/*.xml | wc -l`\n" >> $@
-	@printf "total size  : `ls $(work_dir)/*.xml | xargs du -sch | tail -1 | cut -f1`\n" >> $@
+	@printf "total files : `find $(work_dir)/corpus -name "*.xml" | wc -l`\n" >> $@
+	@printf "total size  : `du -sh $(work_dir)/corpus | cut -f1`\n" >> $@
 
 ############################################
 clean:
+	@echo -n "Are you sure? [y/N] " && read ans && [ $${ans:-N} = y ]
 	rm -rf $(work_dir)
 .PHONY: clean
-
+############################################
+clean/done:
+	find  $(work_dir) -name .done | xargs rm
+.PHONY: clean
--- src/download.py
+++ src/download.py
@@ -14,6 +14,7 @@ class Work:
     def __init__(self, file: str):
         self.file = file
         self.wait_queue = queue.Queue(maxsize=1)
+        self.err = 0
 
     def done(self):
         return self.wait_queue.put(self, block=False)
@@ -30,12 +31,17 @@ class Work:
             if exists(file):
                 file_stats = os.stat(file)
                 if file_stats and file_stats.st_size > 0:
-                    return
+                    return True
             str = loader.get_one(self.file)
             with open(file, "w") as f:
                 f.write(str)
+        except BaseException as err:
+            print("error {}".format(err))
+            self.err += 1
+            return False
         finally:
             self.done()
+        return True
 
 
 def main(argv):
@@ -49,14 +55,18 @@ def main(argv):
     parser.add_argument("--password", nargs='?', required=True, help="Pass")
     parser.add_argument("--domain", nargs='?', required=True, help="Domain")
     parser.add_argument("--n", type=int, nargs='?', default=1, help="Workers count")
+    parser.add_argument("--top", type=int, nargs='?', default=0, help="Take top n docs for each court")
     args = parser.parse_args(args=argv)
 
     cfg = Cfg(args.url, user=args.user, domain=args.domain, password=args.password)
 
     print("court ID: %s" % args.court)
     print("loading ids...")
-    ids = Loader(cfg=cfg).get_list(args.court)
+    ids = get_or_load_ids(Loader(cfg=cfg), args.out_dir, args.court)
     print("got %d docs ids" % len(ids))
+    if args.top > 0:
+        print("take top %d docs of %d" % (args.top, len(ids)))
+        ids = ids[:args.top]
     print("out dir: %s" % args.out_dir)
     print("worker count: %d" % args.n)
     jobs = []
@@ -73,29 +83,63 @@ def main(argv):
         for _i in range(wc):
             job_queue.put(None)
 
-    def start_thread(method):
+    def start_thread(method, add: bool = True):
         thread = threading.Thread(target=method, daemon=True)
         thread.start()
-        workers.append(thread)
+        if add:
+            workers.append(thread)
 
-    start_thread(add_jobs)
+    start_thread(add_jobs, add=False)
+
+    err_lock = threading.Lock()
+    err_count = 0
 
     def start():
+        nonlocal err_count
         while True:
             _j = job_queue.get()
             if _j is None:
                 return
-            _j.do(Loader(cfg=cfg), args.out_dir)
+            if not _j.do(Loader(cfg=cfg), os.path.join(args.out_dir, "corpus", args.court)):
+                with err_lock:
+                    err_count += 1
+            if _j.err > 3:
+                print("too many errors exit worker")
+                break
 
     for i in range(wc):
         start_thread(start)
 
-    with tqdm("downloading", total=len(jobs)) as pbar:
-        for i, j in enumerate(jobs):
-            j.wait()
-            pbar.update(1)
+    def work():
+        with tqdm("downloading", total=len(jobs)) as pbar:
+            for i, j in enumerate(jobs):
+                j.wait()
+                pbar.update(1)
+
+    start_thread(work, add=False)
+
     for w in workers:
         w.join()
+    with err_lock:
+        print("bye with errs {}".format(err_count))
+        if err_count > 0:
+            exit(1)
+
+
+def get_or_load_ids(loader: Loader, dir, court):
+    n_dir = os.path.join(dir, "ids")
+    if not exists(n_dir):
+        os.makedirs(n_dir, exist_ok=True)
+    file = os.path.join(n_dir, court + ".txt")
+    if exists(file):
+        print("File exists: {}".format(file))
+        with open(file, "r") as f:
+            return [line.rstrip() for line in f]
+    res = loader.get_list(court)
+    with open(file, "w") as f:
+        f.write("\n".join(res))
+    print("Wrote file: {}".format(file))
+    return res
 
 
 if __name__ == "__main__":
